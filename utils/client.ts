1faa1a1da0859de6c62e1cb76115e12b445c4301
Mish Ushakov <mishushakov@users.noreply.github.com>
2023-05-03 23:00:46 +0200

    Update

--- utils/client.ts
+++ utils/client.ts
@@ -1,12 +1,13 @@
 import { InferClient } from 'garph/dist/client'
 import { createClient } from '@garph/gqty'
-import { g, queryType, mutationType, subscriptionType } from '../pages/api/[...graphql]'
+import { g } from '../pages/api/[...graphql]'
+import type { queryType, mutationType, subscriptionType } from '../pages/api/[...graphql]'
 
 type ClientTypes = InferClient<{ query: typeof queryType, mutation: typeof mutationType, subscription: typeof subscriptionType }>
 
 export const { useQuery, useMutation, query, mutation, resolved, inlineResolved, useTransactionQuery, useSubscription } = createClient<ClientTypes>({
   schema: g,
-  url: process.env.NODE_ENV === 'production' ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql` : 'http://localhost:3001/api/graphql',
+  url: process.env.NODE_ENV === 'production' ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql` : 'http://localhost:3000/api/graphql',
   defaults: {
     suspense: false
   }
